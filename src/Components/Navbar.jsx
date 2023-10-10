293f9db294afa1da1c14954dbc1367aa42bc3187
unknown <panhareach.phat@student.cadt.edu.kh>
2023-10-10 08:35:49 +0700

    web header and description

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="border-gray-20 mt-20">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <a href="/" className="flex items-center">
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/2560px-Amazon_Web_Services_Logo.svg.png"
                 className="h-14 mr-3"
